5790b0d72d5f731aa39db36b666c17e2d3acca96
coltonsteeve <coltonsteeve@gmail.com>
2019-04-14 22:12:29 -0500

    Added Local Authentication

--- app.js
+++ app.js
@@ -7,6 +7,10 @@ const path = require('path')
 const dotenv = require('dotenv')
 const passport = require('passport')
 const FacebookStrategy = require('passport-facebook').Strategy;
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const LocalStrategy = require('passport-local').Strategy;
+
 
 /*const hostname = '0.0.0.0'    // allows access from remote computers
 const port = 3003;*/
@@ -95,4 +99,58 @@ app.get('/auth/facebook/callback',
   passport.authenticate('facebook', { failureRedirect: '/error' }),
   function(req, res) {
     res.redirect('/chat'); // Was /success
+  });
+
+  // Start of Local Passport Authentication
+  app.use(bodyParser.urlencoded({ extended: true }));
+
+  passport.serializeUser(function(user, cb) {
+    cb(null, user.id);
+  });
+  
+  passport.deserializeUser(function(id, cb) {
+    User.findById(id, function(err, user) {
+      cb(err, user);
+    });
+  });
+
+  /* MONGOOSE SETUP */
+
+mongoose.connect('mongodb://localhost/MyDatabase');
+
+const Schema = mongoose.Schema;
+const UserDetail = new Schema({
+      username: String,
+      password: String
+    });
+const UserDetails = mongoose.model('userInfo', UserDetail, 'userInfo');
+
+/* PASSPORT LOCAL AUTHENTICATION */
+
+passport.use(new LocalStrategy(
+  function(username, password, done) {
+      UserDetails.findOne({
+        username: username
+      }, function(err, user) {
+        if (err) {
+          return done(err);
+        }
+
+        if (!user) {
+          return done(null, false);
+        }
+
+        if (user.password != password) {
+          return done(null, false);
+        }
+        return done(null, user);
+      });
+  }
+));
+
+app.post('/',
+  passport.authenticate('local', { failureRedirect: '/error' }),
+  function(req, res) {
+    //res.redirect('/success?username='+req.user.username);
+    res.redirect('/chat');
   });
